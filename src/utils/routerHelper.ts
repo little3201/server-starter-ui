1f8cbcee136f214cac23e50fe6ff6ecb2b285f95
wq <little3201@163.com>
2024-07-01 15:42:19 +0800

    删除无用代码

--- src/utils/routerHelper.ts
+++ src/utils/routerHelper.ts
@@ -1,11 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import type {
-  Router,
-  RouteRecordRaw,
   RouteLocationNormalized,
   RouteRecordNormalized
 } from 'vue-router'
-import { omit, cloneDeep } from 'lodash-es'
 import { isUrl } from '~/utils'
 
 const Layout = () => import('components/layouts/Index.vue')
@@ -64,72 +60,4 @@ export const pathResolve = (parentPath: string, path: string) => {
   if (isUrl(path)) return path
   const childPath = path.startsWith('/') || !path ? path : `/${path}`
   return `${parentPath}${childPath}`.replace(/\/\//g, '/').trim()
-}
-
-// 路由降级
-export const flatMultiLevelRoutes = (routes: AppRouteRecordRaw[]) => {
-  const modules: AppRouteRecordRaw[] = cloneDeep(routes)
-  for (let index = 0; index < modules.length; index++) {
-    const route = modules[index]
-    if (!isMultipleRoute(route)) {
-      continue
-    }
-    promoteRouteLevel(route)
-  }
-  return modules
-}
-
-// 层级是否大于2
-const isMultipleRoute = (route: AppRouteRecordRaw) => {
-  if (!route || !Reflect.has(route, 'children') || !route.children?.length) {
-    return false
-  }
-
-  const children = route.children
-
-  let flag = false
-  for (let index = 0; index < children.length; index++) {
-    const child = children[index]
-    if (child.children?.length) {
-      flag = true
-      break
-    }
-  }
-  return flag
-}
-
-// 生成二级路由
-const promoteRouteLevel = (route: AppRouteRecordRaw) => {
-  let router: Router | null = createRouter({
-    routes: [route as RouteRecordRaw],
-    history: createWebHistory()
-  })
-
-  const routes = router.getRoutes()
-  addToChildren(routes, route.children || [], route)
-  router = null
-
-  route.children = route.children?.map((item) => omit(item, 'children')) as AppRouteRecordRaw[]
-}
-
-// 添加所有子菜单
-const addToChildren = (
-  routes: RouteRecordNormalized[],
-  children: AppRouteRecordRaw[],
-  routeModule: AppRouteRecordRaw
-) => {
-  for (let index = 0; index < children.length; index++) {
-    const child = children[index]
-    const route = routes.find((item) => item.name === child.name)
-    if (!route) {
-      continue
-    }
-    routeModule.children = routeModule.children || []
-    if (!routeModule.children.find((item) => item.name === route.name)) {
-      routeModule.children?.push(route as unknown as AppRouteRecordRaw)
-    }
-    if (child.children?.length) {
-      addToChildren(routes, child.children, routeModule)
-    }
-  }
 }
