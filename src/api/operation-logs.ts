5eb44d830ba0483b32dee3c92334e6dee09f8635
wq <little3201@163.com>
2024-08-12 08:28:28 +0800

    修改路由

--- src/api/operation-logs.ts
+++ src/api/operation-logs.ts
@@ -1,9 +1,9 @@
 import { api } from '~/boot/axios'
 
-export const retrieveActionLogs = (page: number, size: number) => {
+export const retrieveOperationLogs = (page: number, size: number) => {
   return api.get('/operations-logs', { params: { page: page - 1, size: size } })
 }
 
-export const fetchActionLog = (id: number) => {
+export const fetchOperationLog = (id: number) => {
   return api.get(`/operations-logs/${id}`)
 }
